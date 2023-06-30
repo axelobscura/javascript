012b516d3988964f6729691f3b56adf1173e3b32
axel <axosar@gmail.com>
2023-06-30 12:10:48 -0600

    background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,10 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-row items-center justify-center">
-
       <div className="relative flex flex-row">
         <h1 style={{'fontSize':'7rem','fontWeight':'100'}}>JAVASCRIPT</h1>
         <h3>México</h3>
       </div>
-
-
     </main>
   )
 }
