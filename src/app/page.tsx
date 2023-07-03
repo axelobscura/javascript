21d2e7c9c48fab488e9ecc7b929764fd960b2bc1
axel <axosar@gmail.com>
2023-07-03 13:09:59 -0600

    font-weight

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen">
       <div className="relative flex flex-col items-center justify-center min-h-screen w-screen">
-        <h1 className='color-gray-dark text-9xl font-light font-sans'>JAVASCRIPT</h1>
+        <h1 className='text-gray-dark text-9xl font-light font-sans'>JAVASCRIPT</h1>
         <h3 className='font-sans text-5xl font-bold'>MÉXICO</h3>
+        <p className='text-2xl font-light font-sans text-gray-dark text-center pt-2'>Desbloquea el Poder de JavaScript<br/>Sitios Web con Experiencias Dinámicas e Interactivas</p>
       </div>
     </main>
   )
