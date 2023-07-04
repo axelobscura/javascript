21326f625abf2cbf7250b7fa3042dec1b18673db
axel <axosar@gmail.com>
2023-07-03 19:38:14 -0600

    icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { BsFillRecordCircleFill } from "react-icons/bs";
+import { BsFillRecordCircleFill, BsHCircleFill } from "react-icons/bs";
 
 export default function Home() {
   return (
@@ -6,11 +6,13 @@ export default function Home() {
       <div className="relative flex flex-col items-center justify-center min-h-screen w-screen">
         <h1 className='text-gray-dark text-9xl font-light font-sans'>JAVASCRIPT</h1>
         <div className="flex flex-row items-center">
-          <BsFillRecordCircleFill/>
           <h3 className='font-sans text-5xl font-light p-3'>LATINOAMÉRICA</h3>
-          <BsFillRecordCircleFill/>
         </div>
         <p className='text-2xl font-light font-sans text-gray-dark text-center pt-2'>Desbloquea el Poder de JavaScript<br/>Sitios Web con Experiencias Dinámicas e Interactivas</p>
+        <ul>
+          <li><BsHCircleFill/></li>
+          <li><BsFillRecordCircleFill/></li>
+        </ul>
       </div>
     </main>
   )
