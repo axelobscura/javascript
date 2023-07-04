7ed4313c5c655a7fb5db711b43bf719755de3bd3
axel <axosar@gmail.com>
2023-07-04 13:10:18 -0600

    routers

---
+++ src/app/blog/layout.tsx
@@ -0,0 +1,29 @@
+import '../globals.css'
+import { Rajdhani } from 'next/font/google'
+
+const rajdhani = Rajdhani({ 
+  subsets: ['latin'],
+  weight:["300", "500", "700"],
+  variable: '--font-rajdhani',
+})
+
+export const metadata = {
+  title: 'JAVASCRIPT ARTÍCULOS',
+  description: 'Sitio web de desarrollo de JavaScript: Aprende, crea y domina el mundo de JavaScript. Descubre tutoriales, guías y recursos para desarrollar aplicaciones web, juegos y mucho más. Mantente al día con las últimas técnicas y tendencias en el ámbito del desarrollo de JavaScript.',
+  icons: {
+    icon: '/favicon.png',
+  },
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div className={rajdhani.className}>
+      <h2>Artículos sobre javascript</h2>
+      {children}
+    </div>
+  )
+}
