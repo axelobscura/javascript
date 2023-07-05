cb9648eece5274a22ac985b1eeef7b25976c556f
axel <axosar@gmail.com>
2023-07-04 18:49:52 -0600

    msgs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,6 @@ export default function Home() {
     <main className="flex min-h-screen">
       <div className="relative flex flex-col items-center justify-center min-h-screen w-screen">
         <h1 className='text-gray-dark text-9xl font-light font-sans'>JAVASCRIPT</h1>
-        <div className="flex flex-row items-center">
-          <h3 className='font-sans text-5xl font-light p-3'>LATINOAMÉRICA</h3>
-        </div>
-        <p className='text-2xl font-light font-sans text-gray-dark text-center pt-2'>Desbloquea el Poder de JavaScript<br/>Sitios Web con Experiencias Dinámicas e Interactivas</p>
         <ul className="flex p-3">
           <li className="p-2">
             <Link href="/blog">
