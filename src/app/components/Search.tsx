7afa85cf5d7cdf26310bc84c00c49308cc5d540c
axelobscura <axosar@gmail.com>
2023-07-10 14:32:53 -0600

    tt

--- src/app/components/Search.tsx
+++ src/app/components/Search.tsx
@@ -2,7 +2,7 @@
 export default function Search(){
     return (
         <div className="pr-3">
-            <input type="text" id="large-input" className="block w-full p-4 text-gray-900 border border-gray-dark rounded-lg bg-gray-dark sm:text-md focus:ring-gray-500 focus:border-gray-500 mt-3 mb-3" />
+            <input type="text" id="large-input" className="block w-full p-4 text-gray-900 border border-gray-dark rounded-lg bg-gray-dark sm:text-md focus:ring-gray-500 focus:border-gray-500 mt-3 mb-3" placeholder="Buscar" />
         </div>
     )
 }
