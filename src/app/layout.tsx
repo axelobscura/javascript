dc87dbe252d865d7a41c9ff6658969c62f3e5598
Axel Laurent <axosar@gmail.com>
2026-02-25 15:18:51 -0600

    front

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ const rajdhani = Rajdhani({
 })
 
 export const metadata = {
-  title: 'JAVASCRIPT EN LATINOAMÉRICA',
+  title: 'JAVASCRIPT PROFESIONAL EN MÉXICO - Aprende, Crea y Domina el Mundo de JavaScript',
   description: 'Sitio web de desarrollo de JavaScript: Aprende, crea y domina el mundo de JavaScript. Descubre tutoriales, guías y recursos para desarrollar aplicaciones web, juegos y mucho más. Mantente al día con las últimas técnicas y tendencias en el ámbito del desarrollo de JavaScript.',
   icons: {
     icon: '/favicon.png',
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,11 @@
-import Link from "next/link";
-import { Key, ReactElement, JSXElementConstructor, ReactNode, ReactPortal, PromiseLikeOfReactNode } from "react";
-import MenuUsuario from "./components/MenuUsuario";
-import MenuLateral from "./components/MenuLateral";
-import Search from "./components/Search";
-
-// d6e94ae6348c44ab9b20c475613aae01
-async function getData() {
-  const res = await fetch('https://newsapi.org/v2/everything?q=apple&from=2023-07-05&to=2023-07-05&sortBy=popularity&apiKey=d6e94ae6348c44ab9b20c475613aae01')
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
-  return res.json()
-}
-
 export default async function Home() {
-  const data = await getData();
   return (
-    <div className="container-fluid">
-      <div className="grid grid-cols-12">
-        <div className='col-span-2'>
-          <MenuLateral />
-        </div>
-        <div className='col-span-10 contenido_feat'>
-          <div className="grid grid-cols-12">
-            <div className="col-span-9">
-              <Search />
-            </div>
-            <div className="col-span-3 flex items-center justify-center">
-              <MenuUsuario />
-            </div>
-          </div>
-          <div className="row">
-            <div className="dash">
-              {data.articles.map((dat: { id: Key | null | undefined; title: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | PromiseLikeOfReactNode | null | undefined; body: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | PromiseLikeOfReactNode | null | undefined; url: string; urlToImage: string; }) => {
-                return (
-                  <Link key={dat.id} href={dat.url} target="_blank" className="news">
-                    <div className="news_img">
-                      <img src={dat.urlToImage} alt="javascript en méxico" />
-                    </div>
-                    <p className="text-lg">{dat.title}</p>
-                  </Link>
-                  
-                )
-              })}
-            </div>
-          </div>
+    <div className="h-full container-fluid">
+      <div className="grid items-center justify-center h-full grid-cols-1 text-center">
+        <div className="flex flex-col items-center justify-center h-full text-center">
+          <h2 className="px-20 py-3 text-2xl font-medium text-white bg-black">Celebrando</h2>
+          <h1 className="p-5 font-light text-7xl">30 años de Javascript</h1>
+          <p className="text-2xl">Más de 1,444,723 proyectos en México y el Mundo</p>
         </div>
       </div>
     </div>
